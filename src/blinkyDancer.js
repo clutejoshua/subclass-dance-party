9a2f5023f5da7da47c96bd0fbec8ae41a889e5f4
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-30 19:23:49 -0700

    Succesfully refactored to pseudoclassical style with Anna

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,38 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+};
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  var oldStep = blinkyDancer.step;
+BlinkyDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  this.$node.toggle();
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+  // Generates random number between 0 and 255
+  var randColor = function() {
+    return Math.floor(Math.random() * 256);
   };
 
-  return blinkyDancer;
+  // New CSS background color
+  var styleSettings = {
+    border: '10px solid rgb(' +
+      randColor() + ',' +
+      randColor() + ',' +
+      randColor() + ')'};
+
+  // Sets new CSS background color for this.$node
+  this.$node.css(styleSettings);
 };
+
+// var BlinkyDancer = function (top, left, timeBetweenSteps) {
+//   Dancer.call(this, top, left, timeBetweenSteps);
+// };
+
+// BlinkyDancer.prototype = Object.create(Dancer.prototype);
+// BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+// BlinkyDancer.prototype.step = function() {
+//   Dancer.prototype.step.call(this);
+//   this.$node.toggle();
+// };
