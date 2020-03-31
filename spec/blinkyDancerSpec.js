4726d4d25579fa670b4ff14ced3579c0767f568b
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-30 19:43:51 -0700

    Pseudoclassical style with Anna

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,3 +1,4 @@
+
 describe('blinkyDancer', function() {
 
   var blinkyDancer, clock;
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,31 +8,5 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
   this.$node.toggle();
-
-  // Generates random number between 0 and 255
-  var randColor = function() {
-    return Math.floor(Math.random() * 256);
-  };
-
-  // New CSS background color
-  var styleSettings = {
-    border: '10px solid rgb(' +
-      randColor() + ',' +
-      randColor() + ',' +
-      randColor() + ')'};
-
-  // Sets new CSS background color for this.$node
-  this.$node.css(styleSettings);
 };
 
-// var BlinkyDancer = function (top, left, timeBetweenSteps) {
-//   Dancer.call(this, top, left, timeBetweenSteps);
-// };
-
-// BlinkyDancer.prototype = Object.create(Dancer.prototype);
-// BlinkyDancer.prototype.constructor = BlinkyDancer;
-
-// BlinkyDancer.prototype.step = function() {
-//   Dancer.prototype.step.call(this);
-//   this.$node.toggle();
-// };
